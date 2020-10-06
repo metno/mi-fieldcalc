b12bf873adfa21be3ee2ca2e81e5a5b3822f1153
Alexander Bürger <alexander.buerger@met.no>
2020-10-06 08:00:08 +0200

    version 0.1.4 / debian 0.1.4-1

--- src/mi_fieldcalc/mi_fieldcalc_version.h
+++ src/mi_fieldcalc/mi_fieldcalc_version.h
@@ -32,7 +32,7 @@
 
 #define MI_FIELDCALC_VERSION_MAJOR 0
 #define MI_FIELDCALC_VERSION_MINOR 1
-#define MI_FIELDCALC_VERSION_PATCH 3
+#define MI_FIELDCALC_VERSION_PATCH 4
 
 #define MI_FIELDCALC_VERSION_INT(major,minor,patch) \
     (1000000*major + 1000*minor + patch)
